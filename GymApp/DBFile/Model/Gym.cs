a00ee9991389644ff59b2aff184190d2c5863f55
ac147451 <ac147451@8CG8163L3R>
2025-05-15 11:13:13 +1200

    Add project files.

---
+++ GymApp/DBFile/Model/Gym.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApp.DBFile.Model
+{
+    public class Gym
+    {
+        private int Gym_id { get; set; }
+        private string Gym_name { get; set; }
+        
+        public Gym(int inGym_id, string inGym )
+        {
+            Gym_id = inGym_id;
+            Gym_name = inGym;
+        }
+    }
+}
---
+++ GymApp/Repositories/StorageManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace GymApp.Repositories
+{
+    public class StorageManager
+    {
+        private SqlConnect conn;
+
+
+    }
+}
