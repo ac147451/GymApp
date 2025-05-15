790ef63b70e91c78d9fda56347d2820fe787a3b8
ac147451@avcol.school.nz <ac147451@avcol.school.nz>
2025-05-16 09:50:43 +1200

    Created StorageManager connection to DB and put in try catch

--- GymApp/Repositories/StorageManager.cs
+++ GymApp/Repositories/StorageManager.cs
@@ -11,6 +11,28 @@ namespace GymApp.Repositories
     {
         private SqlConnection conn;
 
+        public StorageManager(string connectionString)
+        {
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+                Console.WriteLine("Connection successful");
+
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Connection NOT successful\n");
+                Console.WriteLine(e.Message);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occured\n");
+                Console.WriteLine(ex.Message);
+
+            }
+        }
 
     }
 }
