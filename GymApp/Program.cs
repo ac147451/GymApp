5cb7aec60f439d666800e0c4011fbc5b53f6f296
ac147451@avcol.school.nz <ac147451@avcol.school.nz>
2025-05-16 09:58:29 +1200

    First attempt at connecting to database

--- GymApp/Program.cs
+++ GymApp/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace GymApp
+﻿using GymApp.Repositories;
+
+namespace GymApp
 {
-    internal class Program
+    public class Program
     {
+        private static StorageManager storageManger;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=\"GymApp Database\";Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+            storageManger = new StorageManager(connectionString);
         }
     }
 }
