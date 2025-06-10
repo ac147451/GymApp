1c77716e32fc5f550907fc295c972f50725def59
ac147451@avcol.school.nz <ac147451@avcol.school.nz>
2025-06-10 13:32:03 +1200

    Added the remaining foreign key fields inside the gym class. Such as countryid, cityid, and suburbid

--- GymApp/DBFile/Model/Gym.cs
+++ GymApp/DBFile/Model/Gym.cs
@@ -11,12 +11,18 @@ namespace GymApp.DBFile.Model
         public int Gym_id { get; set; }
         public string Gym_name { get; set; }
         public string Streetaddress { get; set; }
+        public int Country_id { get; set; }
+        public int City_id { get; set; }
+        public int Suburb_id { get; set; }
         
-        public Gym(int gym_id, string gymName, string streetaddress )
+        public Gym(int gym_id, string gymName, string streetaddress, int country_id, int city_id, int suburb_id)
         {
             Gym_id = gym_id;
             Gym_name = gymName;
             Streetaddress = streetaddress;
+            Country_id = country_id;
+            City_id = city_id;
+            Suburb_id = suburb_id;
         }//Gym Class
     }
 
