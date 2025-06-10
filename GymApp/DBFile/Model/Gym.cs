c4b0ed0d548d31b721f19fee82726064bf99f803
ac147451@avcol.school.nz <ac147451@avcol.school.nz>
2025-06-10 13:38:35 +1200

    added the remaining fields for the member class, such as phonenumber and emailaddress

--- GymApp/DBFile/Model/Gym.cs
+++ GymApp/DBFile/Model/Gym.cs
@@ -31,12 +31,16 @@ namespace GymApp.DBFile.Model
         public int Member_id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        public int Phonenumber { get; set; }
+        public string Emailaddress { get; set; }
 
-        public Member(int member_id, string firstname, string lastname)
+        public Member(int member_id, string firstname, string lastname, int phonenumber, string emailaddress)
         {
             Member_id = member_id;
             Firstname = firstname;
             Lastname = lastname;
+            Phonenumber = phonenumber;
+            Emailaddress = emailaddress;
         }//Member Class
     }
 
