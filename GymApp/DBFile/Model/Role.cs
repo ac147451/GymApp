4f0c9bd7a33d927ee1c2a84468a7543eebae1a39
ac147451@avcol.school.nz <ac147451@avcol.school.nz>
2025-06-11 12:20:39 +1200

    created role class in its own file

---
+++ GymApp/DBFile/Model/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApp.DBFile.Model
+{
+    public class Role
+    {
+        public int Role_id { get; set; }
+        public string Rolename { get; set; }
+
+        public Role(int role_id, string rolename)
+        {
+            Role_id = role_id;
+            Rolename = rolename;
+        }//Role Class
+    }
+}
